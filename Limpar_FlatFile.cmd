0c88feae30c28a9997a3e7f602fc082f9331dc48
cleberq <cleberson.paz@bol.com.br>
2021-04-09 12:26:44 -0300

    up
    ip

---
+++ Limpar_FlatFile.cmd
@@ -0,0 +1,9 @@
+DEl "Z:\BI -\ETL-Projeto(SSIS)\TB_Canal.txt"
+DEl "Z:\BI -\ETL-Projeto(SSIS)\TB_Fabricante.txt" 
+DEl "Z:\BI -\ETL-Projeto(SSIS)\TB_Geografia1.txt" 
+DEl "Z:\BI -\ETL-Projeto(SSIS)\TB_Geografia2.txt" 
+DEl "Z:\BI -\ETL-Projeto(SSIS)\TB_Geografia3.txt" 
+DEl "Z:\BI -\ETL-Projeto(SSIS)\TB_Produto.txt" 
+DEl "Z:\BI -\ETL-Projeto(SSIS)\TB_Promocao.txt" 
+DEl "Z:\BI -\ETL-Projeto(SSIS)\TB_Vendas.txt" 
+pause
